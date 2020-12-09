d64ed1b5bf25e1074f762056e57e5ef795465e95
joosep <joosephook@gmail.com>
2020-12-09 11:24:55 +0000

    Update MultiagentEnv call signature

--- main.py
+++ main.py
@@ -110,11 +110,10 @@ def create_env(scenario_name, scenario_parameters={}):
                         reward_callback=scenario.reward,
                         observation_callback=scenario.observation,
                         discrete_action_input=True,
-                        discrete_action_space=True)
+                        discrete_action_space=True,
+                        episode_limit=25)
     env.observation_structures = calculate_observation_structure(scenario_name, env)
-    # experiment with unshared reward
     env.shared_reward = True
-    env.episode_limit = 25
 
     for a in env.agents:
         a.adversary = getattr(a, 'adversary', False)
@@ -143,13 +142,6 @@ if __name__ == '__main__':
             args = get_commnet_args(args)
         if args.alg.find('g2anet') > -1:
             args = get_g2anet_args(args)
-        # env = MyEnv(map_name=args.map,
-        # env = StarCraft2Env(map_name=args.map,
-        # env = MyEnv(map_name=args.map,
-        #     step_mul=args.step_mul,
-        #     difficulty=args.difficulty,
-        #     game_version=args.game_version,
-        #     replay_dir=args.replay_dir)
         env = create_env('simple_spread', scenario_parameters=dict(num_agents=10, num_landmarks=10))
         env_info = env.get_env_info()
         args.n_actions = env_info["n_actions"]
